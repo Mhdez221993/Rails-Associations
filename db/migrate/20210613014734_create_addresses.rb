6667c64c57eab6bfe408ff0914bc2c5e0184417a
Moises Hernandez <mhdezcoronado@gmail.com>
2021-06-13 10:11:36 +0200

    Add Address & User model with seed data

---
+++ db/migrate/20210613014734_create_addresses.rb
@@ -0,0 +1,12 @@
+class CreateAddresses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :addresses do |t|
+      t.string :street
+      t.string :city
+      t.string :country
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

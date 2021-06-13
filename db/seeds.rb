8106fd28d8d72936eb0bdf698462d48ab7cef5dc
Moises Hernandez <mhdezcoronado@gmail.com>
2021-06-13 02:57:58 +0200

    Add User & Post model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do |index|
+    user = User.create!(name: "User- #{index}")
+    user.posts.create!(body: "This is the post ##{index}")
+end
